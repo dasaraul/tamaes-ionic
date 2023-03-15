3b863af5a66652d4aa092de8112f834e68485daa
dasaraul <acmadauliai@gmail.com>
2023-03-16 04:11:00 +0700

    Ganti css

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'tamaes.ionic.kontak',
   appName: 'kontak',
   webDir: 'www',
   bundledWebRuntime: false
